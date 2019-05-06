2cf5b120b0dee54116387232ecf0936b60bfa4d3
UID <UID@DESKTOP-DACJOCN>
2019-05-06 09:23:02 +0800

    11

---
+++ Test111/src/main/java/com/test1/Jt.java
@@ -0,0 +1,13 @@
+package com.test1;
+
+import java.util.Scanner;
+
+public class Jt {
+	public static void main(String[] args) {
+		int a = new Scanner(System.in).nextInt();
+		for (int i = a-1 ; i > 0 ; i--) {
+			a = a * i ;
+		}
+		System.out.println(a);
+	}
+}
